8f519dd9360dd6c9e2bc377699cb03174401bf9e
knaldhat <knaldhat@gmail.com>
2017-02-14 19:56:11 +0100

    updated required packages

--- ord_mep.R
+++ ord_mep.R
@@ -1,8 +1,11 @@
-#A wrapper around the vegan package to make beautiful ggplot2 ordination plots.
-#The input data list must be loaded with amp_load(), but any OTU table-like matrix
-#(OTU's in rows, sampleID's in columns and abundances in the corresponding cells) alongside metadata
-#for the same samples can be used. Simply choose the desired distance metric and ordination
-#method and a plot is returned.
+#A wrapper around the vegan package to make beautiful ggplot2 ordination plots 
+#for use in microbial ecology profiling. The input data list must be loaded with
+#amp_load() from the ampvis package, but any OTU table-like matrix (OTU's in rows,
+#sampleID's in columns and abundances in the corresponding cells) alongside metadata 
+#for the same samples can be used. Simply choose the desired distance metric and 
+#ordination method and a plot is returned.
+#Required packages:
+#dplyr, ampvis, vegan, 
 ord_mep <- function(
     datalist, #with 3 elements loaded with amp_load: abund, metadata and tax, first two required
     ncomp = 3, 
